e171b1d6eb1ce3b01babbd5383e9fc79b571216d
davi paluch <davipaluch@icloud.com>
2024-06-04 14:12:53 -0300

    tipos de paramentros utilizando express (Route,Query,Body)

--- dockernode/index.js
+++ dockernode/index.js
@@ -5,16 +5,45 @@ const HOST = '0.0.0.0';
 
 const app = express()
 
+app.use(express.json())
+
+
+/*
+
+-- Tipos de parâmetros --
+
+Route Params => Identificar um recurso editar/deletar/buscar (Rota)
+Query Params => Paginação / Filtro (URL)
+Body  Params => Os objetos inserção/alteração (JSON)
+
+*/
+
+
+
 app.get('/', (req, res) => {
+    /*query params (url)*/
+    const query = req.query
+    console.log(query)
+
+    return res.json({message:"hello word!",mimu:"jkl"})
+})
+
+app.get('/courses', (req, res) => {
     return res.json({message:"hello word!",mimu:"jkl"})
 })
 
 app.post("/courses",(req,res)=>{
+    /*body params (objeto enviado no corpo da requisição)*/
+    const body = req.body;
+    console.log(body)
     return res.send("teste");
 })
 
+/*route params (parametro adicionado como rota)*/
 app.put("/courses/:id",(req,res)=>{
-    return res.send("teste");
+    const {id} = req.params
+    console.log(id)
+    return res.json(id);
 })
 
 app.patch("/courses/:id",(req,res)=>{
